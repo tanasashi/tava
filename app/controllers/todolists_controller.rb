3a60d40b6b591bc62d4c29f7b1f965803854fa6e
EC2 Default User <ec2-user@ip-172-31-28-142.ec2.internal>
2021-09-12 03:22:47 +0000

    11

---
+++ app/controllers/todolists_controller.rb
@@ -0,0 +1,47 @@
+class TodolistsController < ApplicationController
+  def new
+     # Viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成する。
+     @list = List.new
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
+  end
+
+  def create
+    # １. データを新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # ２. データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    # 詳細画面へリダイレクト
+    redirect_to todolist_path(list.id)
+  end
+
+  def index
+     @lists = List.all
+
+  end
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to todolists_path
+  end
+
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body, :image)
+  end
+
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '993bbd5edab0d6cf62b0c9200d185a104c03dd378ecbbf7dc840528e99b27499ca7029137b657aa70626cd99529fb0b2922c062859b34f9fa073ee5634759b32'
